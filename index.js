fa93e4a0336837f66f9ed16e81129ba5713e8ca9
Afshin Teymoori <>
2020-03-13 08:36:41 +1100

    add fileOps.js to write to output.txt file

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+
 let a = 10 ;
 const b = 30;
 console.log(a);
-var f = function(){
-    
-}
+var pressButton = document.getElementById("btnPress");
+pressButton.onclick =function () {alert("thanks for clicking me");};
+pressButton.addEventListener("click",function(event){console.log(event);},false);
+// var f = function(){
+//     console.log('it\'s clicked...');
+// }
+// function f1(){
+//     console.log('f1');
+// }
