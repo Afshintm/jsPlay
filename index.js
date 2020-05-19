3d624bc0c97c34643e4af982388e36fef7daf6f1
Afshin Teymoori <>
2020-05-19 14:12:45 +1000

    format document index.json

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+console.log(global);
 
-let a = 10 ;
-const b = 30;
-console.log(a);
 var pressButton = document.getElementById("btnPress");
-pressButton.onclick =function () {alert("thanks for clicking me");};
-pressButton.addEventListener("click",function(event){console.log(event);},false);
-// var f = function(){
-//     console.log('it\'s clicked...');
-// }
-// function f1(){
-//     console.log('f1');
-// }
+console.log(btn)
+pressButton.addEventListener(
+  "click",
+  function (event) {
+    console.log(event);
+  },
+  false
+);
